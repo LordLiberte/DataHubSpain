17a9c1d59784bf29c0ef439512b0535c470e638e
Lord Liberté <GhostRg110@gmail.com>
2025-08-02 19:00:16 +0200

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+
 import streamlit as st
 from opciones import acercade, tables, actualizaciones
 from funciones import data_frames
