3b2950a413de60df3df6737169c52b88080921e8
Lord Liberté <GhostRg110@gmail.com>
2025-08-03 17:02:28 +0200

    Visual changes

--- newVersion/src/components/sidebar.py
+++ newVersion/src/components/sidebar.py
@@ -3,10 +3,10 @@ import streamlit as st
 def create_sidebar():
     st.sidebar.title("🧭 Navegación")
 
-    if st.sidebar.button("🏠 Inicio"):
+    if st.sidebar.button("🏠 Inicio", use_container_width=True):
         st.session_state["page"] = "home"
     
-    if st.sidebar.button("📈 Economía"):
+    if st.sidebar.button("📈 Economía", use_container_width=True):
         st.session_state["page"] = "economy"
         
     if "page" not in st.session_state:
