e47e1ed84609bc4c9c3ebe0b0d60e34005a6fcfa
Lord Liberté <GhostRg110@gmail.com>
2025-08-04 16:54:51 +0200

    Update data_cleaning.py

--- newVersion/src/core/data_cleaning.py
+++ newVersion/src/core/data_cleaning.py
@@ -4,6 +4,6 @@ import numpy as np
 # Elimina los valores NaN
 def delete_none(df):
     # Replace Python's None with numpy's NaN for compatibility with dropna
-    df = df.replace({None: np.nan}, inplace=True)
+    df = df.replace({None: np.nan})
     # Drop columns and then rows that are completely empty
     return df.dropna(axis=1, how='all').dropna(axis=0, how='all')
